db8f4dca6abd441a9698aa188a57bb6248b1124c
akashg30 <akash.gudla@slu.edu>
2025-04-21 17:42:23 +0000

    lab 05 lab 05 is successfully completed.

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express')
 const api = require('./api')
 const middleware = require('./middleware')
 const bodyParser = require('body-parser')
-
-
 // Set the port
 const port = process.env.PORT || 3000
 // Boot the app
@@ -13,12 +11,20 @@ app.use(express.static(__dirname + '/public'));
 // register the routes
 app.use(bodyParser.json())
 app.use(middleware.cors)
+// Register root route
 app.get('/', api.handleRoot)
+// Register Products routes
 app.get('/products', api.listProducts)
 app.get('/products/:id', api.getProduct)
 app.put('/products/:id', api.editProduct)
 app.delete('/products/:id', api.deleteProduct)
 app.post('/products', api.createProduct)
-// Boot the server
-app.listen(port, () => console.log(`Server listening on port ${port}`))
+app.get('/orders', api.listOrders)
+app.get('/orders/', api.createOrder)
+// edit and delete routes
+app.put('/orders/:id', api.editOrder)
+app.delete('/orders/:id', api.deleteOrder)
 
+// Boot the server
+
+app.listen(port, () => console.log(`Server listening on port ${port}`))
